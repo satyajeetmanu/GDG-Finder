c50f455f17d01955e6b68d93ad65b6534177babd
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2022-06-08 09:14:45 +0530

    added a floating action button

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,9 +23,20 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        return view
+        val binding = HomeFragmentBinding.inflate(inflater)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer<Boolean>{
+            shouldNavigate ->
+            if(shouldNavigate == true){
+                binding.root.findNavController().navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
+
+        return binding.root
     }
 }
